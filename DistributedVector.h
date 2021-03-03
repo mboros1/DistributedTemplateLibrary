844860f1fb4850671ee88e74c78618ad2a2fcadc
Martin Boros <mboros04@gmail.com>
2021-03-02 23:37:51 -0500

    Shared memory working properly, added checks for invariant types(require copyable), warning if pointer used, added size constructor, doc strings, test for PoD objects

--- DistributedVector.h
+++ DistributedVector.h
@@ -7,74 +7,125 @@
 
 namespace dtl {
 #include <sys/mman.h>
-
-    const size_t INITIAL_VEC_CAPACITY = 16;
+#include <unistd.h>
 
     template<typename T>
     class DistributedVector {
     private:
+        ///
+        /// \param size size_t size of shared memory; must be page aligned
+        /// \return pointer to allocated shared memory
         T* create_shared_memory(size_t size) {
             int protection = PROT_READ | PROT_WRITE;
-            int visibility = MAP_SHARED | MAP_ANONYMOUS;
+            int visibility = MAP_SHARED;
 
-            // The remaining parameters to `mmap()` are not important for this use case,
-            // but the manpage for `mmap` explains their purpose.
-            void* result = mmap(NULL, size * sizeof(T), protection, visibility, -1, 0);
+            m_shm_fd = memfd_create("shm", 0);
+            ftruncate(m_shm_fd, size * sizeof(T));
+            void* result = mmap(nullptr, size * sizeof(T), protection, visibility, m_shm_fd, 0);
             if (result == MAP_FAILED){
-                std::cerr << "Mapping failed!\n";
+                throw std::runtime_error("Memory mapping failed");
             }
+            file_size = lseek(m_shm_fd, 0, SEEK_END);
             return (T*)result;
         }
 
-        void resize_shared_memory(T* addr, size_t size, size_t new_size){
-            m_begin = (T*)mremap(addr, size * sizeof(T), new_size * sizeof(T), MREMAP_MAYMOVE);
-            if (m_begin == MAP_FAILED) {
-                std::cerr << "Mapping failed!\n";
+        ///
+        /// \param size size_t current size of shared memory
+        /// \param new_size size_t desired size of new memory
+        void resize_shared_memory(size_t size, size_t new_size) {
+            m_begin = (T*)mremap(m_begin, size*sizeof(T), new_size*sizeof(T), MREMAP_MAYMOVE);
+            if (m_begin == MAP_FAILED){
+                throw std::runtime_error("Memory remapping failed");
             }
+            ftruncate(m_shm_fd, new_size * sizeof(T));
             m_end = m_begin + size;
             m_end_cap = m_begin + new_size;
         }
 
+        void check_type_invariants(){
+            if (std::is_pointer<T>::value){
+                std::cout << "WARNING: pointers to heap memory will cause shared memory corruption\n";
+            }
+            if (!std::is_copy_assignable_v<T> && !std::is_copy_constructible_v<T>){
+                throw std::invalid_argument("Template type must be copyable");
+            }
+        }
+
     public:
         DistributedVector() {
-            m_begin = create_shared_memory(INITIAL_VEC_CAPACITY);
-            m_end_cap = m_begin + INITIAL_VEC_CAPACITY;
+            check_type_invariants();
+            m_begin = create_shared_memory(m_page_aligned_vec_size);
+            m_end_cap = m_begin + m_page_aligned_vec_size;
             m_end = m_begin;
         }
 
+        DistributedVector(size_t size){
+            check_type_invariants();
+            size_t alloc_size = m_page_aligned_vec_size;
+            while(alloc_size < 2*size){
+                alloc_size *= 2;
+            }
+            m_begin = create_shared_memory(alloc_size);
+            m_end_cap = m_begin + alloc_size;
+            m_end = m_begin + size;
+        }
+
+        ~DistributedVector(){
+            munmap(m_begin, m_end_cap - m_begin);
+        }
+
+        ///
+        /// \return size_t size of array in use
         size_t size() {
             return m_end - m_begin;
         }
 
+        ///
+        /// \return size_t capacity of allocated shared memory
         size_t capacity() {
             return m_end_cap - m_begin;
         }
 
+        ///
+        /// \return Pointer to underlying shared memory array
         T* data(){
             return m_begin;
         }
 
-        T at(size_t index){
+        ///
+        /// \param index
+        /// \return
+        T& at(size_t index){
             if (index < (m_end - m_begin)) {
                 return *(m_begin+index);
             } else {
-                return -1;
+                throw std::out_of_range("Array bound exception");
             }
         }
 
-        T push_back(T ele){
+        ///
+        /// \param ele element to be added to the vector; ele is copied
+        /// \return index where ele was added
+        size_t push_back(const T& ele){
             if (m_end == m_end_cap){
-                resize_shared_memory(m_begin,size(), 2*capacity());
+                resize_shared_memory(capacity(), 2 * capacity());
             }
-            *m_end = ele;
+            if (std::is_copy_assignable_v<T>) {
+                *m_end = ele;
+            } else if (std::is_copy_constructible_v<T>) {
+                *m_end = T{ele};
+            }
             m_end++;
-            return *m_end;
+            return m_end-1-m_begin;
         }
 
     private:
         T* m_begin;
         T* m_end;
         T* m_end_cap;
+        int m_shm_fd;
+        size_t file_size;
+        const size_t m_page_aligned_vec_size = sysconf(_SC_PAGESIZE) / sizeof(T);
 
     };
 }
--- test/test.cpp
+++ test/test.cpp
@@ -17,14 +17,87 @@ TEST_CASE("Base case", "[base_case]"){
     assert(v.size() == d.size());
 
     for(int i = 0; i < 20000000; ++i){
-        v.push_back(i); d.push_back(i);
+        v.push_back(i);
+        auto j = d.push_back(i);
         auto v_i = v.at(i);
         auto d_i = d.at(i);
         assert(v_i == d_i);
-        if (i % 100 == 0)
-            std::cout << v_i << ":" << d_i << '\n';
+        assert(d.at(j) == d_i);
+        assert(d.size() == v.size());
     }
-    std::cout << '\n';
+
+    for(int i = 0; i < 20000000; ++i){
+        auto d_i = d.at(i);
+        auto x = d.data()[i];
+        assert(x == d_i);
+    }
 
     assert(v.size() == d.size());
+}
+
+TEST_CASE("pre-alloc constructor", "[pre_alloc]"){
+    size_t sz = 200000;
+    std::vector<int> v(sz);
+    dtl::DistributedVector<int> d(sz);
+
+    REQUIRE(v.size() == d.size());
+
+    for(int i = 0; i < sz; ++i){
+        v[i] = i;
+        d.data()[i] = i;
+    }
+    for(int i = sz; i < 3*sz; ++i){
+        v.push_back(i);
+        d.push_back(i);
+    }
+
+    for(int i = 0; i < 3*sz; ++i){
+        auto d_i = d.at(i);
+        auto x = d.data()[i];
+        REQUIRE(x == d_i);
+    }
+
+    REQUIRE(v.size() == d.size());
+}
+
+struct RandomPod {
+    float x=0.5f;
+    float y=0.0f;
+    float z=0.0f;
+    ~RandomPod(){
+        std::cout << "~RandomPod() called\n";
+    }
+};
+
+TEST_CASE("POD object", "[pod_obj]"){
+    dtl::DistributedVector<RandomPod> v;
+
+    {
+        RandomPod p;
+        v.push_back(p);
+        v.push_back(p);
+        v.push_back(p);
+        v.push_back(p);
+        v.push_back(p);
+    }
+
+    for(int i = 0; i < 5; ++i){
+        assert(v.at(i).x == 0.5f);
+    }
+
+    dtl::DistributedVector<RandomPod*> v2;
+}
+
+TEST_CASE("Destructor", "[destructor]"){
+
+    {
+        dtl::DistributedVector<RandomPod> v;
+        RandomPod p;
+        v.push_back(p);
+        v.push_back(p);
+        v.push_back(p);
+        v.push_back(p);
+        v.push_back(p);
+    }
+
 }
